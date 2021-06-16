f99bddf42c1449911c90b1fc078530a4a527d8ac
ManeeshWije <m.mwije1@gmail.com>
2021-06-16 18:14:16 -0400

    deploying

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socket = io("https://maneeshs-chat-app.herokuapp.com/");
+const socket = io("ws://maneeshs-chat-app.herokuapp.com:3000");
 const messageForm = document.getElementById("send-container");
 const messageInput = document.getElementById("message-input");
 const messageContainer = document.getElementById("message-container");
