ae8f4431f34f499cfad16259d4e3980db02be880
ManeeshWije <m.mwije1@gmail.com>
2021-06-16 23:25:38 -0400

    update

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
-const io = require("socket.io")(3000, {
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server, {
   cors: {
     origin: "*",
   },
 });
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index", "./views/index.html");
+});
+
+server.listen(3000);
 
 const users = {};
 
